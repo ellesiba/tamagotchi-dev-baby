8134858998763ca671d650b9c7c9bff78be201ab
ellesiba <rochelle.sibaja@gamil.com>
2023-01-24 22:31:08 -0500

    deploying

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//need three buttons with different events for each
+    //Code
+    //Eat
+    //Sleep ---> will come up with a message that says 'sleep is for the weak'
+    //Touch Grass
+
+    //math.random
+    //math florr for animation
+    function welcome() {
+        alert('Welcome! You are the caretaker of a brand new baby dev!')
+        alert(`This lil friend needs help taking care of its self and that's where YOU come in.`)
+        name = prompt(`First things, first. What is your baby dev's name?`)
+        alert = "That's a fine name! I had an uncle named that."
+    } 
+        
