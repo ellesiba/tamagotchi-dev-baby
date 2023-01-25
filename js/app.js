0e3da51400aed2b2599f84c5a68049f822bcc075
ellesiba <rochelle.sibaja@gamil.com>
2023-01-25 00:46:24 -0500

    start and instruction buttons

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
 //need three buttons with different events for each
     //Code
     //Eat
-    //Sleep ---> will come up with a message that says 'sleep is for the weak'
+    //Sleep
     //Touch Grass
 
     //math.random
     //math florr for animation
     function welcome() {
+        name = prompt(`First things, first. What is your baby dev's name?`)
+        alert(`${name} That's a fine name! I had an uncle named that...`)
+        alert('Good luck with taking care of this little dev~!')
+        alert(`you'll need it...`)
+
+    } 
+    function instructions(){
         alert('Welcome! You are the caretaker of a brand new baby dev!')
         alert(`This lil friend needs help taking care of its self and that's where YOU come in.`)
-        name = prompt(`First things, first. What is your baby dev's name?`)
-        alert = "That's a fine name! I had an uncle named that."
-    } 
-        
+        alert('As a dev caretaker, you will need to make sure your baby dev eats, codes, touches grass, and sleeps.')
+        alert(`Press the feed button to feed you lil dev, press 'code' button to have it code, press the 'touch grass button to let you dev have fun, and press the 'sleep' button to make sure it sleeps...`)
+        alert('must be nice...')
+        alert('Keep track of its stats or you lil dev may die from neglect.')
+        alert('Good luck!')
+    }    
