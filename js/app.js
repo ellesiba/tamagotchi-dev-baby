3104b088547af19775c4c64c7e899badbd31ddaf
ellesiba <rochelle.sibaja@gamil.com>
2023-01-29 01:26:31 -0500

    Finished Tamagotchi

--- js/app.js
+++ js/app.js
@@ -1,92 +1,218 @@
-//need three buttons with different events for each
-    //Code
-    //Eat
-    //Sleep
-    //Touch Grass
-
-    let night = false; //when night is true change visibilty of night screen
-
-class Tamagotchi {
-    constructor(name, hunger, sleep, boredom, age){
-        this.name = name,
-        this.hunger = hunger,
-        this.sleep = sleep,
-        this.boredom = boredom,
-        this.age = age
+//---------------make tamagotchi class----------------
+class BabyDev {
+    constructor(name) {
+        this.name = name;
+        this.hunger = 100;
+        this.coding = 100;
+        this.boredom = 100;
+        this.age = 0;
     }
 }
 
-class BabyDev extends Tamagotchi{
-    constructor(name, hunger, sleep, boredom, age) {
-        super(name, hunger, sleep, boredom, age)
-        this.name = name,
-        this.hunger = hunger,
-        this.sleep = sleep
-        this.boredom = boredom,
-        this.age = age
-    }
-    feed(){
-        this.hunger=+ 10
-    }
-    sleepiness() {
-        this.sleep=+ 10
-    }
-
-    playing() {
-        this.boredom=+ 10
-    }     
+//------------Naming Area------------------------
+function showNamingArea() {
+    document.getElementById("start-button").style.display = "none";
+    document.getElementById("guideBtn").style.display ="none"
+    document.getElementById("naming-area").style.display = "block";
 }
 
-//Baby Dev Start Stats
-    const tama = new BabyDev( " ", 50 , 50 , 0)
+//----------------Create a Baby Dev----------------------
+function babyDevCreation() {
+    let name = document.getElementById("name-input").value;
+    babyDev = new BabyDev(name);
+    document.getElementById("hunger-bar").classList.remove("hidden");
+    document.getElementById("hunger-level").style.width = "100%";
+    document.getElementById("coding-bar").classList.remove("hidden");
+    document.getElementById("coding-level").style.width = "100%";
+    document.getElementById("boredom-bar").classList.remove("hidden");
+    document.getElementById("boredom-level").style.width = "100%";
+    document.getElementById("action-buttons").style.display = "block";
+    document.getElementById("naming-area").style.display = "none";
+    document.getElementById("feed-button").innerHTML = "Feed " + babyDev.name;
+    document.getElementById("coding-button").innerHTML = "Code through the night"
+    document.getElementById("coding-button").addEventListener("click", increaseCoding);
+    document.getElementById("play-button").innerHTML = "Touch Grass! ";
+    document.getElementById("sprite").style.display ="block"
+    displayAge();
+    document.getElementById("coding-button").addEventListener("click", increaseCoding);
+}
 
-    //click --> increase stat
-        //if stat is below or = to 0
-            //death
-        //if stats not zero then age up after interval of 5 seconds
-    //if age 5 age up junior dev
-     //if stat is below or = to 0
-            //death
-    //if age 10 age up potato
-        //if makes it to 15 'wins game'
-        //Nightmode use the red blue button shanger, but change image instead!!
+//---------------------Aging------------------------------
+let aging = setInterval(increaseAge, 1000)
+function increaseAge() {
+    if (babyDev.hunger <= 0 || babyDev.coding <= 0 || babyDev.boredom <= 0) {
+        clearInterval(aging)
+    } else {
+        babyDev.age++;
+        displayAge();
+    }
+}
+    function displayAge() {
+        let age = babyDev.age;
+        if (age === 1) {
+          document.getElementById("age-display").innerHTML = babyDev.name + " is " + age + " year old";
+        } else {
+            document.getElementById("age-display").innerHTML = babyDev.name + " is " + age + " years old";
+          }
+      }
+      let ageCheckInterval = setInterval(checkAge, 1000);
+      function checkAge() {
+          if (babyDev.age === 10) {
+              alert(`Congrats! ${babyDev.name} is now a junior dev!`);
+              changeImage();
+          } if (babyDev.age === 20) {
+              alert(`${babyDev.name} is now a pro--!`);
+              alert("*cough* *cough* I mean POtato!")
+              changeImageTwo();
+          } if (babyDev.age === 30) {
+              winGame()
+          }
+      }
+      function changeImage() {
+          if (babyDev.age === 10) {
+              document.getElementById("sprite").style.display = "none";
+              document.getElementById("sprite-two").style.display = "block";
+          }
+      }
+      function changeImageTwo() {
+          if (babyDev.age === 20) {
+              document.getElementById("sprite-two").style.display = "none";
+              document.getElementById("sprite-three").style.display = "block";
+          }
+      }
+      //----------------Game Win-----------------------
+     let hasWon = false;
+      function winGame() {
+          if (!hasWon && babyDev.age === 30) {
+              alert(`Congrats! ${babyDev.name} a beautiful successful senior potato *sniff*`);
+              hasWon = true;
+              clearInterval(hungerInterval);
+              clearInterval(codingInterval);
+              clearInterval(boredomInterval);
+              clearInterval(aging);
+              document.getElementById("hunger-bar").style.display = "none";
+              document.getElementById("coding-bar").style.display = "none";
+              document.getElementById("boredom-bar").style.display = "none";
+              document.getElementById("action-buttons").style.display = "none";
+              document.getElementById("sprite-three").style.display = "none";
+              document.getElementById("sprite-two").style.display = "none";
+              document.getElementById("sprite-four").style.display = "block";
+          }
+      }
+//----------------Bar Decreases/Increase-----------------------
 
-    
-            
+  let hungerInterval = setInterval(decreaseHungerBar, 2000);
+  let codingInterval = setInterval(decreaseCodingBar, 2000);
+  let boredomInterval = setInterval(decreaseBoredomBar, 2000);
+  function decreaseHungerBar() {
+    if(babyDev.hunger > 0 && babyDev.boredom > 0 && babyDev.coding > 0) {
+        babyDev.hunger -= 10;
+        document.getElementById("hunger-level").style.width = babyDev.hunger + "%";
+    } else {
+        endGame();
+    }
+}
+  function decreaseCodingBar() {
+      if(babyDev.coding > 0 && babyDev.boredom > 0 && babyDev.hunger > 0 ) {
+          babyDev.coding -= 10;
+          document.getElementById("coding-level").style.width = babyDev.coding + "%";
+      } else {
+          endGame()
+      }
+  }
+  function decreaseBoredomBar() {
+    if(babyDev.boredom > 0 && babyDev.coding > 0 && babyDev.hunger > 0 ) {
+    babyDev.boredom -= 10;
+    document.getElementById("boredom-level").style.width = babyDev.boredom + "%"
+  } else {
+   endGame()
+}
+}
+function increaseHungerBar() {
+if(babyDev.hunger < 100) {
+babyDev.hunger += 10;
+document.getElementById("hunger-level").style.width = babyDev.hunger + "%"
+}
+}
+function increaseCodingBar() {
+    if(babyDev.coding < 100) {
+      babyDev.coding += 10;
+      document.getElementById("coding-level").style.width = babyDev.coding + "%";
+    }
+}
+function increaseBoredomBar() {
+    if(babyDev.boredom < 100) {
+    babyDev.boredom += 10;
+    document.getElementById("boredom-level").style.width = babyDev.boredom + "%";
+    }
+    }
+    function increaseCoding() {
+        babyDev.coding = 100;
+        document.getElementById("coding-level").style.width = babyDev.coding + "%";
+        let timeout = setTimeout(function() {
+        document.getElementById("feed-button").disabled = false;
+        document.getElementById("play-button").disabled = false;
+        document.getElementById("coding-button").disabled = false;
+        }, 2000);
+        document.getElementById("feed-button").disabled = true;
+        document.getElementById("play-button").disabled = true;
+        document.getElementById("coding-button").disabled = true;
+        clearInterval(codingInterval);
+        codingInterval = setInterval(decreaseCodingBar, 2000); {
+            changeBackgroundToNight()
+            setTimeout(function(){
 
+            changeBackgroundToDay()
+            }, 2000);
 
-//link to the start of game or expand from here...
+        }
+        }
+//-------------------------Endings---------------------------
+     function endGame() {
+        let hasWon = true
+        clearInterval(hungerInterval);
+        clearInterval(codingInterval);
+        clearInterval(boredomInterval);
+        clearInterval(aging);
+        document.getElementById("hunger-bar").style.display = "none";
+        document.getElementById("coding-bar").style.display = "none";
+        document.getElementById("boredom-bar").style.display = "none";
+        document.getElementById("action-buttons").style.display = "none";
+        document.getElementById("sprite").style.display = "hidden";
+        document.getElementById("sprite-two").style.display = "none";
+        document.getElementById("sprite-three").style.display = "none";
+        document.getElementById("sprite-four").style.display = "none";
+        
+        babyDev = null;
+            alert(`Oh no! Your baby dev got burnt out!`);
+            document.getElementById("dead").style.display = "block";
+        }
+//------------------Change BG-----------------------------
+        const changeBackgroundToNight = () => {
+            document.body.style.backgroundImage = "url('https://i.imgur.com/jC6aDiW.gif')";
+        }
+        
+        const changeBackgroundToDay = () => {
+            document.body.style.backgroundImage = "url('https://i.imgur.com/ZXw1qCi.gif')";
+        }
+//------------------Clear Interface-----------------------------
 
-//Function Junction
-
-
-function welcome() {
-    name = prompt(`First things, first. What is your baby dev's name?`, 'Carlos')
-    alert(`${name}? That's a fine name! I had an titi named that...`)
-    alert('Good luck with taking care of this little dev~!')
-    alert(`you'll need it...`)
-} 
-//at end change visibilty of start to hidden
-
-
-//Instruction
-
-function instructions(){
+        function clearOut(){
+            clearInterval(hungerInterval);
+            clearInterval(codingInterval);
+            clearInterval(boredomInterval);
+            clearInterval(aging);
+            document.getElementById("hunger-bar").style.display = "none";
+            document.getElementById("coding-bar").style.display = "none";
+            document.getElementById("boredom-bar").style.display = "none";
+            document.getElementById("action-buttons").style.display = "none";
+        }
+//-----------------Instruction----------------------------
+        function instructions(){
     alert('Welcome! You are the caretaker of a brand new baby dev!')
-    alert(`This lil friend needs help taking care of its self and that's where YOU come in.`)
-    alert('As a dev caretaker, you will need to make sure your baby dev eats, codes, touches grass, and sleeps.')
-    alert(`Press the feed button to feed you lil dev, press 'code' button to have it code, press the 'touch grass button to let you dev have fun, and press the 'sleep' button to make sure it sleeps...`)
-    alert('must be nice...')
-    alert('Keep track of its stats or you lil dev may die from neglect.')
+    alert(`This lil friend needs help taking care of itself and that's where YOU come in.`)
+    alert('As a dev caretaker, you will need to make sure your baby dev eats, codes, and every once in a while touches grass')
+    alert(`Press the feed button to feed your lil dev, press 'code' button to have it code, and press the 'touch grass button to let your dev have fun`)
+    alert('Keep track of its stats or you lil dev may burnout from neglect.')
     alert('Good luck!')
 }  
-//at after startBtn click change visibilty to hidden
-
-// Game Start----------------------
-
-function GameStart(){
-    welcome();
-    function statsHunger() {
-        const feedStat = document.getElementById('.hunger');
-    }
-}
